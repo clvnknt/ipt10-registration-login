16bc86a460bb63c23e5f2e47c0a44a96837c7be9
clvnknt <p.calvinkent@gmail.com>
2022-11-16 19:31:14 +0800

    first commit

--- login-form.php
+++ login-form.php
@@ -4,15 +4,25 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Login Form</title>
+	<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-<h1>Login Form</h1>
+<div class="card">
+  <h5 class="card-header">Login Form</h5>
+  <div class="card-body">
+    <h5 class="card-title">Please login!</h5>
 
-
-
-
-
-
-
+	<form method="POST" action="login.php">
+		<label class="form-label" for="sample-identifier">Username/Email Address</label>
+		<input type="text" id="sample-identifier" class="form-control" name="identifier"/>
+		<label class="form-label" for="sample-password">Email</label>
+		<input type="password" id="sample-password" class="form-control" name="password"/>
+		
+		<br><button type="submit" class="btn btn-primary" name="login-user">Submit</button>
+		<a class="btn btn-primary" href="registration-form.php" role="button">Go To Registration Form</a>
+	</form>
+</div>
+</div>
 </body>
 </html>
--- registration-form.php
+++ registration-form.php
@@ -4,15 +4,29 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Registration Form</title>
+	<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-<h1>Registration Form</h1>
+<div class="card">
+  <h5 class="card-header">Registration Form</h5>
+  <div class="card-body">
+    <h5 class="card-title">Register now!</h5>
 
-
-
-
-
-
-
+	<form method="POST" action="register.php">
+		<label class="form-label" for="sample-username">Username</label>
+		<input type="text" id="sample-username" class="form-control" name="username"/>
+		<label class="form-label" for="sample-email">Email</label>
+		<input type="email" id="sample-email" class="form-control" name="email"/>
+		<label class="form-label" for="sample-password">Password</label>
+		<input type="password" id="sample-password" class="form-control" name="password"/>
+		<label class="form-label" for="confirm-password">Confirm Password</label>
+		<input type="password" id="confirm-password" class="form-control" name="confirm-password"/>
+		
+		<br><button type="submit" class="btn btn-primary" name="register-user">Submit</button>
+		<a class="btn btn-primary" href="login-form.php" role="button">Go To Login Form</a>
+	</form>
+</div>
+</div>
 </body>
 </html>
